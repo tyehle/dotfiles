129110bbd785ad44ef14f261fc6f7877b8342a11
Tobin Yehle <tobinyehle@gmail.com>
2020-09-19 22:30:43 -0600

    add fish config

---
+++ fish/config/fish/config.fish
@@ -0,0 +1,21 @@
+# change the intro message
+function fish_greeting
+    # fortune
+end
+
+# don't shorten directory names in the prompt
+set -g fish_prompt_pwd_dir_length 0
+
+# setup rbenv
+status --is-interactive; and rbenv init - | source
+
+# setup pyenv
+pyenv init - | source
+
+# setup powerline
+set fish_function_path $fish_function_path "$HOME/.pyenv/versions/3.8.3/lib/python3.8/site-packages/powerline/bindings/fish"
+powerline-setup
+set VIRTUAL_ENV_DISABLE_PROMPT 1
+
+# aliases
+alias g='git'

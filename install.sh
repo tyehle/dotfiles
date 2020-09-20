e164721d6a64c729f19a0ace82de869aaa88b2ed
Tobin Yehle <tobinyehle@gmail.com>
2020-09-19 22:35:37 -0600

    make the install script more chatty

--- install.sh
+++ install.sh
@@ -5,18 +5,22 @@ base_dir=$(dirname "$(readlink -f "$0")")
 shopt -s nullglob
 shopt -s dotglob
 
+echo "linking to $HOME/"
 for home_dir in "$base_dir"/*/home
 do
     for file in "$home_dir"/*
     do
+        echo "  $(basename "$file")"
         ln -sf "$file" "$HOME/"
     done
 done
 
+echo "linking to $HOME/.config/"
 for config_dir in "$base_dir"/*/config
 do
     for file in "$config_dir"/*
     do
+        echo "  $(basename "$file")"
         ln -sf "$file" "$HOME/.config/"
     done
 done

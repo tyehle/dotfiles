4041c22553f68e9014ea7b69e511f308b23f258a
Tobin Yehle <tobinyehle@gmail.com>
2020-09-21 14:20:15 -0600

    don't clutter fish_user_paths with repeated adds in config.fish

--- fish/config/fish/config.fish
+++ fish/config/fish/config.fish
@@ -7,14 +7,16 @@ end
 set -g fish_prompt_pwd_dir_length 0
 
 # setup rbenv
+contains $HOME/.rbenv/bin $fish_user_paths; or set --universal --append --export fish_user_paths $HOME/.rbenv/bin
 status --is-interactive; and rbenv init - | source
 
 # setup pyenv
-set -Ux PYENV_ROOT $HOME/.pyenv
-set -Ux fish_user_paths $PYENV_ROOT/bin $fish_user_paths
+set --universal --export PYENV_ROOT $HOME/.pyenv
+contains $PYENV_ROOT/bin $fish_user_paths; or set --universal --append --export fish_user_paths $PYENV_ROOT/bin
 pyenv init - | source
 
 # setup powerline
+powerline-daemon -q
 set fish_function_path $fish_function_path "$HOME/.pyenv/versions/3.8.3/lib/python3.8/site-packages/powerline/bindings/fish"
 powerline-setup
 set VIRTUAL_ENV_DISABLE_PROMPT 1

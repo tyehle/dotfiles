86b2e74184401255393d7790374549c96b3cd368
Tobin Yehle <tobinyehle@gmail.com>
2023-05-17 17:33:45 -0600

    update some things for mac

--- fish/config/fish/config.fish
+++ fish/config/fish/config.fish
@@ -3,6 +3,9 @@ function fish_greeting
     # fortune
 end
 
+eval (/opt/homebrew/bin/brew shellenv)
+fish_add_path  "$HOME/.cargo/bin/"
+
 # don't shorten directory names in the prompt
 set -g fish_prompt_pwd_dir_length 0
 
@@ -16,9 +19,9 @@ contains $PYENV_ROOT/bin $fish_user_paths; or set --universal --append --export 
 pyenv init - | source
 
 # setup powerline
-powerline-daemon -q
-set fish_function_path $fish_function_path "$HOME/.pyenv/versions/3.8.3/lib/python3.8/site-packages/powerline/bindings/fish"
-powerline-setup
+function fish_prompt
+    powerline-rs --shell bare $status
+end
 set VIRTUAL_ENV_DISABLE_PROMPT 1
 
 # aliases
